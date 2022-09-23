3da3dbaf868cbcf5b1f8648c24aa37ec99f43658
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-09-23 00:13:23 -0700

    imported files from lab 3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = express
+'use strict';
+
+require('dotenv').config();
+
+const { sequelizeDatabase } = require('./src/models');
+const { start } = require('./src/server.js');
+
+sequelizeDatabase.sync()
+  .then(() => {
+    console.log('Successful Connection!');
+    start();
+  })
+  .catch(err => console.error(err));
